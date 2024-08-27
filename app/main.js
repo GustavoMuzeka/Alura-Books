8c295efc7cddd9f30bd54a48c329c0e7bad8ed51
GUSTAVO MUZEKA <gustavo@suzuki.dominio>
2024-08-27 16:02:44 -0300

    Ultimo COMMIT, projeto finalizado

--- app/main.js
+++ app/main.js
@@ -1,30 +1,10 @@
-const html = document.querySelector('#livros')
-
 let livros = []
-const endpointdaAPI = "https://guilhermeonrails.github.io/casadocodigo/livros.json"
+const endpointDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'
 getBuscarLivrosDaAPI()
 
 async function getBuscarLivrosDaAPI() {
-    const res = await fetch(endpointdaAPI)
+    const res = await fetch(endpointDaAPI)
     livros = await res.json()
-    livros.forEach((livro) => { 
-        if (livro.quantidade < 1){
-            livros.disponibilidade = 'indisponivel'
-        }else{
-            livros.disponibilidade = 'disponivel'
-        }
-        html.innerHTML += `<div class="livro">
-      <img class="livro__imagens ${livros.disponibilidade}" src="${livro.imagem}"
-        alt="${livro.alt}" />
-      <h2 class="livro__titulo">
-        ${livro.titulo}
-      </h2>
-      <p class="livro__descricao">${livro.autor}</p>
-      <p class="livro__preco" id="preco">R$${livro.preco}0</p>
-      <div class="tags">
-        <span class="tag">${livro.categoria}</span>
-      </div>
-    </div>`
-    })
+    livros = aplicarDesconto(livros)
+    exibirOsLivrosNaTela(livrosComDesconto)
 }
-
